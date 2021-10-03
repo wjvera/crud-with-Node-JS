10ea887c86812bd04fef69393101cfb551acf193
Washington <washington666xxx@gmail.com>
2021-10-02 21:44:39 -0500

    crud finalizado

--- config/yargs.js
+++ config/yargs.js
@@ -1,40 +1,28 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea'
+}
 
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca completado o pendiente'
+}
 
 const argv = require('yargs')
                 .command('crear', 'Crear un elemento por hacer', {
-                    descripcion: {
-                        demand: true,
-                        alias: 'd',
-                        desc: 'Descripcion de la tarea'
-                    }
+                    descripcion
                 })
 
                 .command('actualizar', 'Actualiza el estado completado de una tarea', {
-                    descripcion: {
-                        demand: true,
-                        alias: 'd',
-                        desc: 'Descripcion de la tarea'
-                    },
-
-                    completado: {
-                        default: true,
-                        alias: 'c',
-                        desc: 'Marca completado o pendiente'
-                    }
+                    descripcion,
+                    completado
                 })
 
                 .command('borrar', 'borra un elemento de la bd', {
-                    descripcion: {
-                        demand: true,
-                        alias: 'd',
-                        desc: 'Descripcion de la tarea'
-                    },
-
-                    completado: {
-                        default: true,
-                        alias: 'c',
-                        desc: 'Marca completado o pendiente'
-                    }
+                    descripcion,
+                    completado
                 })
 
                 .help()
